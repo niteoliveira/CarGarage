39f086bb8c6e730ac9f1949f89e1979df5236d95
niteoliveira <toodigital2023@gmail.com>
2025-09-23 18:56:13 -0300

    Teste:Criacao da classe usuario com o ORM direto no app.py

--- Backend/app.py
+++ Backend/app.py
@@ -1,11 +1,30 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
-@app.route('/')
-def home():
-    return 'Olá, Flask! Minha primeira rota.'
+db = SQLAlchemy(app)
 
-#ponto de entrada para rodar o servidor
+class Usuario(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    nome = db.Column(db.String(50), nullable=False)
+    email = db.Column(db.String(120), unique=True, nullable=False)
+    bloqueado = db.Column(db.Boolean, nullable=False, default=False)
+    def __repr__(self):
+        return f"Usuario(id={self.id}, nome='{self.nome}', email='{self.email}')"
+
+@app.route('/usuarios')
+def gerenciar_usuarios():
+    novo_usuario = Usuario(nome='Joao Silva', email='joao@exemplo.com')
+    db.session.add(novo_usuario)
+    db.session.commit()
+    usuarios = Usuario.query.all()
+    lista_de_usuarios = [f'{u.id} - {u.nome} - {u.email} - Bloqueado: {u.bloqueado}' for u in usuarios]
+    return "<h1>Usuários:</h1>" + "<br>".join(lista_de_usuarios)
+
 if __name__ == '__main__':
+    with app.app_context():
+        db.create_all()
     app.run(debug=True)
